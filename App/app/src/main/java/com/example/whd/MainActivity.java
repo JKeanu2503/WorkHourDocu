30c6abb6945d8d4835a15945a12288f21535d25e
JKeanu2503 <keanu.hock@gmail.com>
2026-05-28 13:56:17 +0200

    clean project from sample code

--- App/app/src/main/java/com/example/whd/MainActivity.java
+++ App/app/src/main/java/com/example/whd/MainActivity.java
@@ -7,21 +7,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView gui_TextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.gui_TextView = (TextView) this.findViewById(R.id.gui_TextView);
-        int x = Integer.parseInt((String)this.gui_TextView.getText());
 
-        char c = 'A';
-        String s = "Auto liadufhglikydjfbngvlksdyjhbfnvlijkdfyhgblnkjfd \n ";
-
-        Auto a = new Auto("DA E 2307", 160000);
-
-        String ss = "X " + a.getKm_stand();
     }
 }
